b77837b43832d2f667136809a1ed85c5b1c5e836
Vypolor <m9LHQbTU>
2022-02-20 21:26:29 +0300

    init

--- Server/src/main/java/ComandHandler.java
+++ Server/src/main/java/ComandHandler.java
@@ -1,31 +1,38 @@
 import controllers.TaskController;
 import exceptions.NotFoundException;
 import transport.Reqest;
+import transport.Response;
 
 import java.io.IOException;
 import java.sql.Time;
 import java.util.UUID;
 
 public class ComandHandler {
+    private static TaskController taskController = new TaskController();
+    private static String userId;
 
-    public static void parseCommand(Reqest reqest) throws IOException, NotFoundException {
-        TaskController controller = new TaskController();
+    public static Response parseCommand(Reqest reqest) throws IOException, NotFoundException {
 
         switch (reqest.getCommand()){
             case "add":
-                controller.sheduleTask(reqest.getUserId(), reqest.getTaskName(), reqest.getTaskDesc(), reqest.getAlertTime());
-                break;
+                taskController.sheduleTask(reqest.getUserId(), reqest.getTaskName(), reqest.getTaskDesc(), reqest.getAlertTime());
+                return null;
+                /*return new Response();*/
             case "delete":
-                controller.deleteTask(reqest.getTaskId());
-                break;
+                taskController.deleteTask(reqest.getTaskId());
+                return null;
             case "update":
-                controller.updateTask(reqest.getTaskId(), reqest.getTaskName(), reqest.getTaskDesc());
-                break;
+                taskController.updateTask(reqest.getTaskId(), reqest.getTaskName(), reqest.getTaskDesc());
+                return null;
             case "unshedule":
-                controller.unsheludeTask(reqest.getTaskId());
-                break;
+                taskController.unsheludeTask(reqest.getTaskId());
+                return null;
             default: throw new IllegalArgumentException("invalid case");
         }
 
     }
+
+    /*private static String createResponse() {
+        taskController.getAllTasks();
+    }*/
 }
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,10 +1,18 @@
+import controllers.TaskController;
+import controllers.TimerScheduler;
 import exceptions.NotFoundException;
+import model.Task;
 import transport.Reqest;
+import transport.Response;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.sql.Time;
+import java.util.List;
+import java.util.Timer;
+import java.util.UUID;
 
 public class Server extends Thread {
     private Socket socket;
@@ -21,16 +29,37 @@ public class Server extends Thread {
     }
 
     public void run() {
+        TimerScheduler scheduler = getTimer();
+        ComandHandler comandHandler = new ComandHandler();
         while (true) {
             try {
                 Reqest reqest = (Reqest) ois.readObject();
-                ComandHandler comandHandler = new ComandHandler();
-                comandHandler.parseCommand(reqest);
+                Response response = comandHandler.parseCommand(reqest);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException | NotFoundException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private TimerScheduler getTimer() {
+        TaskController controller = new TaskController();
+        List<Task> taskList = null;
+        try {
+            taskList = controller.getAllTasks(UUID.fromString("f5643624-0494-492c-9225-7cc576b1fdef"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Time timeTemplate = new Time(999, 59, 60);
+        Task executedTask = null;
+        for(Task cur : taskList) {
+            if(cur.getAlert().getAlertTime().getTime() < timeTemplate.getTime()) {
+                executedTask = cur;
+                timeTemplate = cur.getAlert().getAlertTime();
+            }
+        }
+        TimerScheduler scheduler = new TimerScheduler(3);
+        return scheduler;
     }
 }
